1bd14b786ac284ab9ad7d1a49ec54045d492420a
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2019-05-06 17:59:16 +0000

    Ajustes no layout

--- resources/views/accounts/mode_view/table/tfoot.blade.php
+++ resources/views/accounts/mode_view/table/tfoot.blade.php
@@ -1,29 +1,29 @@
 <tr class="title">
-    <th colspan="5">
+    <th colspan="5" style="border-left: 2px solid;">
         {{__('accounts.totals-paid')}}:
     </th>
     @for($month=0; $month<12; $month++)
-        <th class="{{$isNowYear && $month==$period->actual->month?'bg-dark':''}} text-right" colspan="2">
+        <th class="{{$isNowYear && $month==$period->actual->month?'bg-dark':''}} text-right" colspan="2" style="border-left: 2px solid;">
             {!! formatMoney($totals->paid[$month]) !!}
         </th>
     @endfor
 </tr>
 <tr class="title">
-    <th colspan="5">
+    <th colspan="5" style="border-left: 2px solid;">
         {{ __('accounts.totals-not-paid') }}:
     </th>
     @for($month=0; $month<12; $month++)
-        <th class="{{ $isNowYear && $month==$period->actual->month ? 'bg-dark' : '' }} text-right" colspan="2">
+        <th class="{{ $isNowYear && $month==$period->actual->month ? 'bg-dark' : '' }} text-right" colspan="2" style="border-left: 2px solid;">
             {!!formatMoney($totals->nonPaid[$month])!!}
         </th>
     @endfor
 </tr>
 <tr class="title">
-    <th colspan="5">
+    <th colspan="5" style="border-left: 2px solid;">
         {{__('accounts.totals')}}:
     </th>
     @for($month=0; $month<12; $month++)
-        <th class="{{ $isNowYear && $month == $period->actual->month ? 'bg-dark' : '' }} text-right" colspan="2">
+        <th class="{{ $isNowYear && $month == $period->actual->month ? 'bg-dark' : '' }} text-right" colspan="2" style="border-left: 2px solid;">
             {!! formatMoney($totals->nonPaid[$month] + $totals->paid[$month]) !!}
         </th>
     @endfor
